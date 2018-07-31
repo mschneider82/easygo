986e569e58d2d2bd9d4e5a5d7c3cf43d0a56ced3
Matthias Schneider <ms@wck.biz>
2018-07-31 14:50:04 +0200

    added new test

--- randomutils_test.go
+++ randomutils_test.go
@@ -3,7 +3,13 @@ package easygo
 import "testing"
 
 func TestRandomIDwithPrefix(t *testing.T) {
+	// Test1 with random result
 	if len(RandomIDwithPrefix("bla", 10)) != 13 {
 		t.Errorf("unexpected length of RandomIDwithPrefix")
+	}
+	// Test 2 with static result because we only have one Letter to choice
+	RandomLetters = []rune("a")
+	if RandomIDwithPrefix("bla", 10) != "blaaaaaaaaaaa" {
+		t.Errorf("unexpected, should be blaaaaaaaaaaa, got: %v", RandomIDwithPrefix("bla", 10))
 	}
 }
